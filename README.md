d833e2bcbdb6dd6d7faedc1bc8fba8fc91ef4fa4
mikebcbc <mikebconstantino@gmail.com>
2017-12-12 16:36:59 -0500

    README and small fixes

---
+++ README.md
@@ -0,0 +1,12 @@
+# Mike Constantino's Portfolio
+
+## Introduction
+
+This is my portfolio as of late 2017/early 2018. Redesigned for simplicity and consistently updated with skills, projects, etc.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- jQuery
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Chart
-
 var ctx = $("#skillsChart");
 var Chart = new Chart(ctx, {
   type: 'bar',
@@ -36,3 +35,33 @@ var Chart = new Chart(ctx, {
       }
   }
 });
+
+// Scroll all nicely
+$('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
